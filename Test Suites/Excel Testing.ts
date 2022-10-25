f74ab9080f6505927d3e375485728c4a2e034afb
justi <justi@Justins-PC>
2022-10-25 09:59:01 -0400

    Example Test

---
+++ Test Suites/Excel Testing.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Excel Testing</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>0ea06e12-fa06-4318-85be-ee91905f9978</testSuiteGuid>
+   <testCaseLink>
+      <guid>14e571d7-9782-40de-ad27-bf297a573758</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Excel Test 1</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>13a52cda-54c2-4d06-9f75-9a53450e3373</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Excel Files/Rep File</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>13a52cda-54c2-4d06-9f75-9a53450e3373</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Numbers</value>
+         <variableId>925502b7-057e-4b2b-a72a-78bb8d901ccd</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>13a52cda-54c2-4d06-9f75-9a53450e3373</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Character</value>
+         <variableId>a8283f2a-813e-4d65-b8ec-c9e79bdafc94</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>13a52cda-54c2-4d06-9f75-9a53450e3373</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Symbol</value>
+         <variableId>34ad7dd7-43d0-47fd-9f15-0fa4593083b4</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
